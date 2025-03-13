fec25a68773fc6df59889a88fd90f01520c0e0a6
Omar Aflak <aflakomar@gmail.com>
2025-03-13 00:09:29 +0000

    Delete unused files

--- src/pages/TranscriptionPage.jsx
+++ src/pages/TranscriptionPage.jsx
@@ -10,7 +10,7 @@ export default function TranscriptionPage() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <Center style={{ height: '100vh', width: '100vw' }}>
+    <Center style={{ height: '100vh' }}>
       <Stack>
         <ApiKeyCard
           apiKey={apiKey}
